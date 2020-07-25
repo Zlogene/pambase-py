7d64d8232f9e337b86d294f11d403a81e972159b
Mikle Kolyada <zlogene@gentoo.org>
2020-07-25 18:27:06 +0300

    sync
    Signed-off-by: Mikle Kolyada <zlogene@gentoo.org>

--- pambase.py
+++ pambase.py
@@ -19,12 +19,6 @@ def main():
 	parser.add_argument('--krb5', action="store_true", help='enable pam_krb5.so module')
 	parser.add_argument('--minimal', action="store_true", help='install minimalistic PAM stack')
 
-	# Settings which can change behaviour
-	# TODO: Maybe use a different argument type here vs modules? e.g. --add-modules?
-	parser.add_argument('--debug', action="store_true", help='add debug option to the stack')
-	parser.add_argument('--unix-extended-encryption', action="store_true")
-	parser.add_argument('--likeauth', action="store_true")
-
 	parsed_args = parser.parse_args()
 	processed = process_args(parsed_args)
 
@@ -34,30 +28,23 @@ def process_args(args):
 	blank_variables = ["krb5_authtok", "unix_authtok", "unix_extended_encryption",
 					   "likeauth", "nullok"]
 
-	# Create a blank dictionary
-	# Then add in our parsed args
+	# create a blank dictionary
+	# then add in our parsed args
 	output = dict.fromkeys(blank_variables, "")
 	output.update(vars(args))
 
-	# basic-conf logic
 	if args.krb5 and args.passwdqc:
 		output["krb5_authtok"] = "use_authtok"
 
 	if args.krb5 or args.passwdqc:
 		output["unix_authtok"] = "use_authtok"
 
-	if args.debug:
-		output["debug"] = "debug"
-
-	# TODO: noop (it's a noop in pambase too)
-	if args.unix_extended_encryption:
-		pass
-
-	if args.nullok:
-		output["nullok"] = "nullok"
+	# uncondtional variables
+	output["debug"] = "debug"
+	output["nullok"] = "nullok"
 
 	if args.krb5:
-		output["krb5_params"] = "{0} ignore_root try_first_pass".format("debug" if args.debug else "").strip()
+		output["krb5_params"] = "{0} ignore_root try_first_pass".format("debug").strip()
 
 	return output
 
--- templates/login.tpl
+++ templates/login.tpl
@@ -1,9 +1,8 @@
 {% if securetty -%}
 auth       required     pam_securetty.so
 {% endif -%}
-
 auth       include	system-local-login
 account	   include	system-local-login
 password   include	system-local-login
-session	   optional     pam_lastlog.so {{ debug|default('', true) }}
+session	   optional     pam_lastlog.so {{ debug }}
 session	   include      system-local-login
