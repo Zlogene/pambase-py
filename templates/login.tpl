Zlogene/pambase-py

commit fb84d6762243b9c488226a07e8d14b4041cc2944

Formatting fixes
Signed-off-by: Sam James <sam@gentoo.org>

templates/login.tpl
@@ -1,9 +1,10 @@
 {% if securetty -%}
-auth       required     pam_securetty.so
+auth	required	pam_securetty.so
 {% endif -%}
 
 auth       include	system-local-login
-account	   include	system-local-login
+account    include	system-local-login
 password   include	system-local-login
-session	   optional     pam_lastlog.so {{ debug|default('', true) }}
-session	   include      system-local-login
+session    optional pam_lastlog.so {{ debug|default('', true) }}
+session    include	system-local-login
+

templates/other.tpl
@@ -2,3 +2,4 @@ auth       required	pam_deny.so
 account    required	pam_deny.so
 password   required	pam_deny.so
 session    required	pam_deny.so
+

templates/passwd.tpl
@@ -2,4 +2,5 @@ auth		sufficient	pam_rootok.so
 auth		include		system-auth
 account		include		system-auth
 password	include		system-auth
--password	optional	pam_gnome_keyring.so  {{ unix_authok }}
+-password	optional	pam_gnome_keyring.so {{ unix_authtok }}
+

templates/su.tpl
@@ -1,8 +1,9 @@
-auth       sufficient		pam_rootok.so
-auth       required		pam_wheel.so use_uid
+auth       sufficient	pam_rootok.so
+auth       required     pam_wheel.so use_uid
 auth       include		system-auth
 account    include		system-auth
 password   include		system-auth
 session    include		system-auth
-session    required		pam_env.so
+session    required     pam_env.so
 session    optional		pam_xauth.so
+

templates/system-login.tpl
@@ -1,5 +1,5 @@
 auth		required	pam_shells.so {{ debug|default('', true) }}
-auth		required	pam_nologin.so
+auth		required	pam_nologin.so 
 auth		include		system-auth
 {% if not minimal -%}
 auth            required        pam_faillock.so preauth silent audit deny=3 unlock_time=600
@@ -8,7 +8,7 @@ auth            [default=die]   pam_faillock.so authfail audit deny=3 unlock_tim
 {% endif -%}
 
 account		required	pam_access.so {{ debug|default('', true) }}
-account		required	pam_nologin.so
+account		required	pam_nologin.so 
 account		include		system-auth
 {% if not minimal -%}
 account         required        pam_faillock.so

pambase.py
@@ -47,7 +47,7 @@ def process_args(args):
 
 	# unconditional variables
 	output["likeauth"] = "likeauth"
-	output["unix_authok"] = "use_authok"
+	output["unix_authtok"] = "use_authtok"
 
 	if args.debug:
 		output["debug"] = "debug"
@@ -89,7 +89,7 @@ def parse_templates(processed_args):
 			rendered_template = template.render(processed_args)
 
 			if rendered_template:
-				output.write(rendered_template + "\r\n")
+				output.write(rendered_template + "\n")
 
 
 if __name__ == "__main__":

templates/system-auth.tpl
@@ -10,8 +10,8 @@ auth        [success=1 default=ignore]      pam_krb5.so {{ krb5_params }}
 auth		required	pam_unix.so try_first_pass {{ likeauth }} {{ nullok|default('', true) }} {{ debug|default('', true) }}
 auth		optional	pam_permit.so
 {% if not minimal -%}
-auth            required	pam_faillock.so preauth silent audit deny=3 unlock_time=600
-auth            sufficient	pam_unix.so {{ nullok|default('', true) }} try_first_pass
+auth            required        pam_faillock.so preauth silent audit deny=3 unlock_time=600
+auth            sufficient      pam_unix.so {{ nullok|default('', true) }} try_first_pass
 auth            [default=die]   pam_faillock.so authfail audit deny=3 unlock_time=600
 {% endif -%}
 
@@ -34,6 +34,7 @@ password	[success=1 default=ignore]	pam_krb5.so {{ krb5_params }}
 
 password	required	pam_unix.so try_first_pass {{ unix_authtok|default('', true) }} {{ nullok|default('', true) }} {{ unix_extended_encryption|default('', true) }} {{ debug|default('', true) }}
 password	optional	pam_permit.so
+
 {% if pam_ssh -%}
 session		optional	pam_ssh.so
 {% endif -%}

commit 8c6c3dc897b4282ac84bde0e026560c04c0751db

Consistent formatting
In fb84d67, we made some changes to fix formatting, and
matched some of legacy pambase's formatting even where it was
inconsistent to be sure that things would work.

With the fixed line endings in that commit (\n, not \r\n),
we're all good, so let's be more consistent with tabs.

Fixes: fb84d6762243b9c488226a07e8d14b4041cc2944
Signed-off-by: Sam James <sam@gentoo.org>

templates/login.tpl
@@ -1,10 +1,9 @@
 {% if securetty -%}
-auth	required	pam_securetty.so
+auth		required	pam_securetty.so
 {% endif -%}
 
-auth       include	system-local-login
-account    include	system-local-login
-password   include	system-local-login
-session    optional pam_lastlog.so {{ debug|default('', true) }}
-session    include	system-local-login
-
+auth		include		system-local-login
+account		include		system-local-login
+password	include		system-local-login
+session		optional 	pam_lastlog.so {{ debug|default('', true) }}
+session		include		system-local-login

templates/other.tpl
@@ -1,5 +1,4 @@
-auth       required	pam_deny.so
-account    required	pam_deny.so
-password   required	pam_deny.so
-session    required	pam_deny.so
-
+auth		required	pam_deny.so
+account		required	pam_deny.so
+password   	required	pam_deny.so
+session    	required	pam_deny.so

templates/passwd.tpl
@@ -3,4 +3,3 @@ auth		include		system-auth
 account		include		system-auth
 password	include		system-auth
 -password	optional	pam_gnome_keyring.so {{ unix_authtok }}
-

templates/su.tpl
@@ -1,9 +1,8 @@
-auth       sufficient	pam_rootok.so
-auth       required     pam_wheel.so use_uid
-auth       include		system-auth
-account    include		system-auth
-password   include		system-auth
-session    include		system-auth
-session    required     pam_env.so
-session    optional		pam_xauth.so
-
+auth		sufficient	pam_rootok.so
+auth		required	pam_wheel.so use_uid
+auth		include		system-auth
+account		include		system-auth
+password	include		system-auth
+session		include		system-auth
+session		required	pam_env.so
+session		optional	pam_xauth.so

templates/system-login.tpl
@@ -1,5 +1,5 @@
 auth		required	pam_shells.so {{ debug|default('', true) }}
-auth		required	pam_nologin.so 
+auth		required	pam_nologin.so
 auth		include		system-auth
 {% if not minimal -%}
 auth            required        pam_faillock.so preauth silent audit deny=3 unlock_time=600
@@ -8,7 +8,7 @@ auth            [default=die]   pam_faillock.so authfail audit deny=3 unlock_tim
 {% endif -%}
 
 account		required	pam_access.so {{ debug|default('', true) }}
-account		required	pam_nologin.so 
+account		required	pam_nologin.so
 account		include		system-auth
 {% if not minimal -%}
 account         required        pam_faillock.so